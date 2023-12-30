c77b19332eb04276101f7fa76d305c755a4040d3
MaxGanske <maxganske14@gmail.com>
2023-12-29 18:04:16 -0800

    Finished first version of portfolio

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("running");
 
 const homeInfo = document.getElementById("homeInfo");
 const experiencesInfo = document.getElementById("experiencesInfo");
+const projectsInfo = document.getElementById("projectsInfo");
 
 const profilePicture = document.getElementById("profilePicture");
 const initialInfoText = document.getElementById("initialInfoText");
@@ -10,45 +11,55 @@ const topSectionBottom = document.getElementById("topSectionBottom");
 const aboutMeSection = document.getElementById("homeInfo");
 
 //Experiences
-const experiences = document.getElementById("experiencesInfo");
 const experience1 = document.getElementById("experience1");
 const experience2 = document.getElementById("experience2");
 const experience3 = document.getElementById("experience3");
 
+//Projects
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+const project3 = document.getElementById("project3");
+
+//Pages
+let homeOpen = true;
+let experiencesOpen = false;
+let projectsOpen = false;
+
 function experiencesTab() {
- 
-      //   homeInfo.classList.add("is-hidden");
-      //  experiencesInfo.classList.remove("is-hidden");
-        initialInfoText.classList.remove("reverse-Inital-text-info");
-        profilePicture.classList.remove("reverse-profile-picture");
-        profilePicture.classList.add("minimize-img");
-        initialInfoText.classList.add("minimize-initial-text");
 
-        setTimeout(()=> {
-            topSection.classList.remove("move-initial-Background-down");
-            topSectionBottom.classList.remove("move-Initial-section-bottom-down");
-            topSection.classList.add("move-initial-Background-up");
-            topSectionBottom.classList.add("move-Initial-section-bottom-up");
-        }, 1000);
+    let animTime = 1200;
+        if(homeOpen){
+            minimizeInitialInfo();
 
-         setTimeout(() => {
-            aboutMeSection.classList.remove("move-about-me-reverse");
-            aboutMeSection.classList.add("move-about-me");
-        }, 1100); 
+        }
+        if(projectsOpen) {
+            moveProjectsAwayExperiences();
+            animTime = 300;
+        }
+
         setTimeout(() => {
-            experiences.classList.remove("is-hidden");
+            experiencesInfo.classList.remove("is-hidden");
             experience1.classList.add("move-experience-left-to-right");
             experience2.classList.add("move-experience-right-to-left");
             experience3.classList.add("move-experience-left-to-right");
-        }, 1200);
+        }, animTime);
         setTimeout(() => {
             aboutMeSection.classList.add("is-hidden");
         }, 2000);
+
+        homeOpen = false;
+        experiencesOpen = true;
+        projectsOpen = false;
 }
 
 function homeTab(){
 
-       moveExperiencesAway();
+       if(experiencesOpen){
+        moveExperiencesAway();
+       }
+       if(projectsOpen) {
+        moveProjectsAwayHome();
+       }
        aboutMeSection.classList.remove("is-hidden");
        aboutMeSection.classList.remove("move-about-me");
        aboutMeSection.classList.add("move-about-me-reverse");
@@ -64,6 +75,40 @@ function homeTab(){
            profilePicture.classList.add("reverse-profile-picture");
        }, 700);
     }, 500);
+
+       homeOpen = true;
+       experiencesOpen = false;
+       projectsOpen = false;
+}
+
+function projectsTab() {
+
+       if(homeOpen) {
+        minimizeInitialInfo();
+
+        setTimeout(() => {
+            projectsInfo.classList.remove("is-hidden");
+            project1.classList.add("move-experience-left-to-right");
+            project2.classList.add("move-experience-right-to-left");
+            project3.classList.add("move-experience-left-to-right");
+        }, 1200);
+        setTimeout(() => {
+            aboutMeSection.classList.add("is-hidden");
+        }, 2000);
+
+       }
+       if(experiencesOpen) {
+          moveExperiencesAway();
+            projectsInfo.classList.remove("is-hidden");
+            project1.classList.add("move-experience-right-to-left");
+            project2.classList.add("move-experience-left-to-right");
+            project3.classList.add("move-experience-right-to-left");
+       }
+
+       homeOpen = false;
+       experiencesOpen = false;
+       projectsOpen = true;
+
 }
 
 function moveExperiencesAway(){
@@ -80,6 +125,58 @@ function moveExperiencesAway(){
         experience1.classList.remove("move-experience-left-to-right-reverse");
         experience2.classList.remove("move-experience-right-to-left-reverse");
         experience3.classList.remove("move-experience-left-to-right-reverse");
-        experiences.classList.add("is-hidden");
+        experiencesInfo.classList.add("is-hidden");
+    }, 500);
+}
+
+function moveProjectsAwayHome(){
+    project1.classList.add("move-experience-left-to-right-reverse");
+    project2.classList.add("move-experience-right-to-left-reverse");
+    project3.classList.add("move-experience-left-to-right-reverse");
+
+    setTimeout(()=> {
+        project1.classList.remove("move-experience-left-to-right");
+        project2.classList.remove("move-experience-right-to-left");
+        project3.classList.remove("move-experience-left-to-right");
+
+        project1.classList.remove("move-experience-left-to-right-reverse");
+        project2.classList.remove("move-experience-right-to-left-reverse");
+        project3.classList.remove("move-experience-left-to-right-reverse");
+        projectsInfo.classList.add("is-hidden");
+    }, 500);
+}
+function moveProjectsAwayExperiences(){
+    project1.classList.add("move-experience-right-to-left-reverse");
+    project2.classList.add("move-experience-left-to-right-reverse");
+    project3.classList.add("move-experience-right-to-left-reverse");
+
+    setTimeout(()=> {
+
+        project1.classList.remove("move-experience-right-to-left-reverse");
+        project2.classList.remove("move-experience-left-to-right-reverse");
+        project3.classList.remove("move-experience-right-to-left-reverse");
+        projectsInfo.classList.add("is-hidden");
     }, 1500);
 }
+
+function minimizeInitialInfo() {
+
+        initialInfoText.classList.remove("reverse-Inital-text-info");
+        profilePicture.classList.remove("reverse-profile-picture");
+        profilePicture.classList.add("minimize-img");
+        initialInfoText.classList.add("minimize-initial-text");
+
+        setTimeout(()=> {
+            topSection.classList.remove("move-initial-Background-down");
+            topSectionBottom.classList.remove("move-Initial-section-bottom-down");
+            topSection.classList.add("move-initial-Background-up");
+            topSectionBottom.classList.add("move-Initial-section-bottom-up");
+        }, 1000);
+
+        setTimeout(() => {
+            aboutMeSection.classList.remove("move-about-me-reverse");
+            aboutMeSection.classList.add("move-about-me");
+        }, 1100);
+        
+}
+
